95c5f8d28ceb154097328d8c9854b2f8b0da9a8c
UnknownHacker1125 <34523791+UnknownHacker1125@users.noreply.github.com>
2019-10-17 17:41:03 -0400

    Update contributors.ml

--- contributors.ml
+++ contributors.ml
@@ -9,7 +9,6 @@
 
 [UnknownHacker1125](http://github.com/UnknownHacker1125)
 [Ankit kumar](https://github.com/PrajapatiAnkit)
-
 [MariaMich}(https://github.com/MariaMich)
 [hiranyagarbh](http://github.com/hiranyagarbh)
 Aditi T(http://github.com/mystic-potato)
